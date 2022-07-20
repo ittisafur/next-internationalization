aa9e3799c37a3a8744f22f34d4998a2a48847a67
ittisafur <ittisafur@gmail.com>
2022-07-20 22:33:25 +0600

    native features

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
 import type { NextPage } from 'next'
+import { Fragment } from 'react';
 
 const Home: NextPage = () => {
+
+      const regionNamesInFrench= new Intl.DisplayNames(['fr'], { type: 'region' });
   return (
-    <h1>Hello World</h1>
+    <Fragment>
+      <h1>Hello World</h1>
+      <button onClick={() => alert(`United States in French is ${regionNamesInFrench.of('US')}`)
+        }>
+        Click me and i'll pop open
+      </button>
+    </Fragment>
   )
 }
 
